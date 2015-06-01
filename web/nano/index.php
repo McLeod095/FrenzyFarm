3d06b956c092d55588fd56693a1055e4f7e19f94
Dergachev Sergey <mcleod095@gmail.com>
2015-06-01 16:48:37 +0300

    тестовый рисунок

---
+++ web/nano/index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<link href="/style/main.css" rel="stylesheet"/>
+</head>
+<body>
+	<div class="scene">
+		<div class="board">
+			<div id="d1" class="out right"></div>
+			<div id="d2" class="out right"></div>
+			<div id="d3" class="out right"></div>
+			<div id="d4" class="out right"></div>
+			<div id="d5" class="out right"></div>
+			<div id="d6" class="out right"></div>
+			<div id="d7" class="out right"></div>
+		</div>
+	</div>
+</body>
+</html>
