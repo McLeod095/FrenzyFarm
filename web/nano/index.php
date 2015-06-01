2a736b0d5f8a35eb914cc82a731dcf868d1e06f4
Dergachev Sergey <mcleod095@gmail.com>
2015-06-01 17:19:46 +0300

    Add many div

--- web/nano/index.php
+++ web/nano/index.php
@@ -5,13 +5,37 @@
 <body>
 	<div class="scene">
 		<div class="board">
-			<div id="d1" class="out right"></div>
-			<div id="d2" class="out right"></div>
-			<div id="d3" class="out right"></div>
-			<div id="d4" class="out right"></div>
-			<div id="d5" class="out right"></div>
-			<div id="d6" class="out right"></div>
-			<div id="d7" class="out right"></div>
+			<div id="d1"	class="out left"></div>
+			<div id="d2"	class="out left"></div>
+			<div id="d3"	class="out left"></div>
+			<div id="d4"	class="out left"></div>
+			<div id="d5"	class="out left"></div>
+			<div id="d6"	class="out left"></div>
+			<div id="d7"	class="out left"></div>
+			<div id="d8"	class="out left"></div>
+			<div id="d9"	class="out left"></div>
+			<div id="d10"	class="out left"></div>
+			<div id="d11"	class="out left"></div>
+			<div id="d12"	class="out left"></div>
+			<div id="d13"	class="out left"></div>
+			<div id="d14"	class="out left"></div>
+			<div id="d15"	class="out left"></div>
+
+			<div id="d16"	class="out right"></div>
+			<div id="d17"	class="out right"></div>
+			<div id="d18"	class="out right"></div>
+			<div id="d19"	class="out right"></div>
+			<div id="d20"	class="out right"></div>
+			<div id="d21"	class="out right"></div>
+			<div id="d22"	class="out right"></div>
+			<div id="d23"	class="out right"></div>
+			<div id="d24"	class="out right"></div>
+			<div id="d25"	class="out right"></div>
+			<div id="d26"	class="out right"></div>
+			<div id="d27"	class="out right"></div>
+			<div id="d28"	class="out right"></div>
+			<div id="d29"	class="out right"></div>
+			<div id="d30"	class="out right"></div>
 		</div>
 	</div>
 </body>
